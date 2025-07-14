843b48da7bd4822090aeedcd1329292851ef567f
yamaguchi-a <yamagucchi-a@pise.co.jp>
2025-07-14 10:53:32 +0900

    各種設定変更

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** import type { NextConfig } from "next";*/
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: "export",
-};
+  output: 'export',
 
-module.exports = nextConfig;
+}
+ 
+module.exports = nextConfig
