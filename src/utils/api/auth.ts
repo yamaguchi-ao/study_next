5f9f9f5fb5ce880389b14cf27db12587f8d38c14
yamaguchi-a <yamaguchi-a@pise.co.jp>
2025-08-18 14:28:38 +0900

    本番用URL変更

--- src/utils/api/auth.ts
+++ src/utils/api/auth.ts
@@ -1,6 +1,6 @@
 // 新規登録API呼び出し
 export const signUp = async (userData: { username: string, email: string, password: string }) => {
-    const url = "http://localhost:3000/api/auth/signup";
+    const url = "https://yamaguchi-ao.github.io/study_next/api/auth/signup";
 
     const data = await fetch(url, {
         method: "POST",
@@ -11,7 +11,7 @@ export const signUp = async (userData: { username: string, email: string, passwo
 
 // ログインAPI呼び出し
 export const signIn = async (loginData: { email: string, password: string }) => {
-    const url = "http://localhost:3000/api/auth/signin";
+    const url = "https://yamaguchi-ao.github.io/study_next/api/auth/signin";
 
     const data = await fetch(url, {
         method: "POST",
