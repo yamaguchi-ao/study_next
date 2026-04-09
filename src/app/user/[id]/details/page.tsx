1c98a3269c8b9bfd9b63c0f61215143dbec340d4
yamaguchi-a <yamaguchi-a@pise.co.jp>
2026-04-09 16:29:31 +0900

    ユーザー詳細に自身の投稿一覧とコメント一覧をリスト表示してタブで切り替えられるように作成

--- src/app/user/[id]/details/page.tsx
+++ src/app/user/[id]/details/page.tsx
@@ -3,15 +3,15 @@ import { getUserData } from "@/app/actions/user-action";
 import { Sidebar } from "@/components/layout/sidebar"
 import { ReturnButton, UpdateButton } from "@/components/ui/button";
 import { redirect } from "next/navigation";
+import DataList from "./dataList";
 
 // ユーザー詳細
 export default async function UserDetails({ params }: { params: Promise<{ id: Number }> }) {
 
     const userId = (await params).id;
-    const data = await getUserData(userId);
-
     const cookies = await getCookies();
 
+    // cookieがない場合はログイン画面にリダイレクト
     if (cookies === null) {
         return redirect("/login?error=true");
     }
@@ -25,25 +25,38 @@ export default async function UserDetails({ params }: { params: Promise<{ id: Nu
         return redirect("/post");
     }
 
+    // IDからユーザーデータを取得
+    const data = await getUserData(userId);
+
+    if (!data) {
+        // 
+        return redirect("/post");
+    }
+
+    const posts = data.posts;
+    const comments = data.comments;
+
     return (
         <>
             <title>ユーザー 詳細</title>
             <div className="flex h-main overflow-hidden">
                 <Sidebar />
-                <div className="w-full p-7 flex flex-col justify-center items-center">
-                    <div className="w-1/2">
-                        <div className="flex pb-10 items-center">
-                            <div className="w-1/2">ユーザー名：</div>
-                            <h1 className="text-2xl pl20">{data.name}</h1>
+                <div className="w-full p-7 flex flex-col ">
+                    <div className="w-full">
+                        <div className="flex justify-evenly pb-9">
+                            <div className="flex items-center">
+                                <div className="pl-2.5">ユーザー名：</div>
+                                <h1 className="text-2xl pl20">{data.name}</h1>
+                            </div>
+                            <div className="flex items-center">
+                                <div className="pl-2.5">メールアドレス：</div>
+                                <h1 className="text-2xl pl20">{data.email}</h1>
+                            </div>
                         </div>
-                        <div className="flex pb-10 items-center">
-                            <div className="w-1/2">メールアドレス：</div>
-                            <h1 className="text-2xl pl20">{data.email}</h1>
-                        </div>
-                        <div className="flex pb-10 items-center">
-                            <div className="w-1/2">パスワード：</div>
-                            <h1 className="text-2xl pl20">**********</h1>
-                        </div>
+
+                        {/* 自身の投稿と自身がコメントしたリスト */}
+                        <DataList posts={posts} comments={comments} />
+
                         <div className="flex justify-between">
                             <ReturnButton />
                             <UpdateButton type={"user"} id={userId} />
